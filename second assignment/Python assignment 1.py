c2578a732570887a07552d7a6b80ca3c3a61e48c
Malakh101 <82877096+Malakh101@users.noreply.github.com>
2021-05-12 14:05:09 +0100

    Add files via upload

---
+++ second assignment/Python assignment 1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+number= int("1000")
+type(number)
+
+
+# In[2]:
+
+
+number = int(3.764)
+type(number)
+
+
+# In[17]:
+
+
+expenses = 99.45
+expenses =str(99.45)
+expenses= "$"+ str(99.45)+"dollars"
+expenses
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+A boolean is a value that is either true or false.
+TRUE OR FALSE are the two types
+
+
+# In[ ]:
+
+
+The % operator is called modulo operator
+It divides a number and brings back the remainder
+
+
+# In[26]:
+
+
+fancy_name = 'Malak'
+print(fancy_name)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ second assignment/python assignment 2.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+def easy_money():
+    return (100)
+easy_money()
+
+
+# In[6]:
+
+
+def best_food_ever():
+    return ("Sushi")
+best_food_ever()
+
+
+# In[5]:
+
+
+def convert_to_currency(int):
+    return ("$"+str(int))
+convert_to_currency(5)
+    
+
+
+# In[36]:
+
+
+def string_adder(a=str,b=str ):
+    if a and b is ( ):
+        return ( )
+    else:
+         return(a + " " + b)
+
+string_adder("Hello", "World")
+        
+   
+    
+    
+
+
+# In[23]:
+
+
+def long_word_function(str):
+    if len(str) > 7:
+        return (True)
+    else:
+        return(False)
+long_word_function("python")
+    
+
+
+# In[33]:
+
+
+def same_first(str):
+    if str[0]== str[-1]:
+        return(True)
+    else:
+        return(False)
+    
+    
+same_first("runner")
+    
+
+
+# In[35]:
+
+
+def first_three_characters(str):
+    return(str[0:3])
+first_three_characters("dynasty")
+
+
+# In[ ]:
+
+
+
+
